09bb185f56261a9e85a09e7ca7fdfdb13b8f27e9
angeli <angelimvictor01@gmail.com>
2024-10-29 14:20:11 -0400

    Update spaceGame.py
    add win and lose conditions

--- spaceGame.py
+++ spaceGame.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import pygame
 import random
 
@@ -14,6 +16,7 @@ pygame.display.set_caption("Spaceship fallen")
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
+green =(0,128,0)
 
 # variáveis do jogador
 player_width = 60
@@ -39,13 +42,18 @@ obj_height = 30
 obj_x = random.randint(0, screen_width - obj_width)  # random spawn
 obj_y = -obj_height
 obj_speed = 2  # randomizer speed 
+max_speed_obj = 10
 
 # score
 score = 0
+score_limit = 10
 
 # var de controle do jogo
 playing = True  # quando falso o jogo para 
 clock = pygame.time.Clock()
+win = False
+game_over=False
+
 
 # criar o jogador
 player = Player()
@@ -58,28 +66,35 @@ while playing:
     for event in pygame.event.get():  # verifica eventos no jogo, como o fechamento da janela 
         if event.type == pygame.QUIT:
             playing = False
+    # verifica status do jogo
+    if game_over:   
+        text= font.render("Game over! press R to restart", True, red)
+        screen.blit(text,(screen_width //2 -text.get_width()//2, screen_height//2))
+    elif win:
+        text = font.render("Victory! press R to play again", True, green)
+        screen.blit(text,(screen_width//2 - text.get_width()//2, screen_height//2))
+
 
     # movimentação do jogador
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and player.rect.x > 0:
-        player.rect.x -= player_speed
-    if keys[pygame.K_RIGHT] and player.rect.x < screen_width - player_width:
-        player.rect.x += player_speed
-    if keys[pygame.K_UP] and player.rect.y > 0:
-        player.rect.y -= player_speed
-    if keys[pygame.K_DOWN] and player.rect.y < screen_height - player_height:
-        player.rect.y += player_speed
+    else:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.rect.x > 0:
+            player.rect.x -= player_speed
+        if keys[pygame.K_RIGHT] and player.rect.x < screen_width - player_width:
+            player.rect.x += player_speed
+        if keys[pygame.K_UP] and player.rect.y > 0:
+            player.rect.y -= player_speed
+        if keys[pygame.K_DOWN] and player.rect.y < screen_height - player_height:
+            player.rect.y += player_speed
 
     # posição do objeto
     obj_y += obj_speed  # faz com que o objeto caia em vertical
 
     # posição do objeto reset
     if obj_y > screen_height:
-        obj_y = -obj_height
-        obj_x = random.randint(0, screen_width - obj_width)
-
-    # colisão 
-    if (player.rect.x < obj_x + obj_width and 
+        game_over = True
+        
+    elif (player.rect.x < obj_x + obj_width and 
         player.rect.x + player_width > obj_x and 
         player.rect.y < obj_y + obj_height and 
         player.rect.y + player_height > obj_y):
@@ -87,6 +102,12 @@ while playing:
         obj_y = -obj_height
         obj_x = random.randint(0, screen_width - obj_width)
         obj_speed += 1
+    if obj_speed > max_speed_obj:
+        obj_speed = max_speed_obj
+    
+    #condição de vitoria
+    if score > score_limit:
+        win= True
 
     # desenhar jogador
     screen.blit(player.image, player.rect)
@@ -98,6 +119,18 @@ while playing:
     font = pygame.font.Font(None, 30)
     text = font.render("score: " + str(score), True, white)
     screen.blit(text, (10, 10))
+
+    # press R 
+    if win or game_over:
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_r]:
+            #reset logic
+            game_over = False
+            win = False
+            score = 0
+            obj_speed= 2
+            obj_y = -obj_height
+            player.rect.center=(screen_width //2, screen_height - player.image.get_height()-10)
 
     # tela e fps
     pygame.display.flip()
